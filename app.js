7b6427e06faa6bc5bfd3606fad36419c54392792
Shaheer Shakir <shaheershakir22@gmail.com>
2023-09-28 00:45:07 +0530

    configured submit route

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ if (mongoose.connection.readyState === 1) {
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -96,12 +97,41 @@ app.get("/register", function(req, res) {
 })
 
 app.get("/secrets", (req, res) => {
+    User.find({"secret": {$ne: null}}, (err, foundUsers) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    });
+});
+
+app.get("/submit", (req, res) => {
     if (req.isAuthenticated()) {
-    res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
 });
+
+app.post("/submit", (req, res) => {
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id)
+    .then (foundUser => {
+        foundUser.secret = submittedSecret;
+        foundUser.save()
+        .then (() => {
+            res.redirect("/secrets")
+        })
+    }).catch ((err)=> {
+        console.log(err);
+    })
+})
 
 app.get('/logout', function(req, res, next){
     req.logout(function(err) {
